9d625a7a2e280f8aea3dc6bd525ddc6c79f252a8
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2022-06-03 14:06:31 +0200

    final version with comments

--- monitor_manago.py
+++ monitor_manago.py
@@ -11,11 +11,11 @@ def change_resolution():
     primary_monitor = win32api.EnumDisplayDevices()
 
     # get devmodew object containing monitor information
-    settings = win32api.EnumDisplaySettings(primary_monitor.DeviceName, -1)
+    devmodew = win32api.EnumDisplaySettings(primary_monitor.DeviceName, win32con.ENUM_CURRENT_SETTINGS)
 
     # check current resolution
-    height = settings.PelsHeight
-    width = settings.PelsWidth
+    height = devmodew.PelsHeight
+    width = devmodew.PelsWidth
 
     # set variables for new resolution
     # if the resolution is set to 1920x1080, change it to 1280x720
@@ -27,19 +27,15 @@ def change_resolution():
         new_width = 1920
         new_height = 1080
 
-    # execute of the resolution change
-    # get the devmode object
-    devmode = pywintypes.DEVMODEType()
-
     # assign variables to settings options
-    devmode.PelsWidth = new_width
-    devmode.PelsHeight = new_height
+    devmodew.PelsWidth = new_width
+    devmodew.PelsHeight = new_height
 
     # fill the options fields of devmode object
-    devmode.Fields = win32con.DM_PELSWIDTH | win32con.DM_PELSHEIGHT
+    devmodew.Fields = win32con.DM_PELSWIDTH | win32con.DM_PELSHEIGHT
 
     # use windows api to pass the display settings written to devmode object
-    win32api.ChangeDisplaySettings(devmode, 0)
+    win32api.ChangeDisplaySettings(devmodew, 0)
 
 
 def change_refresh_rate():
@@ -47,10 +43,10 @@ def change_refresh_rate():
     primary_monitor = win32api.EnumDisplayDevices()
 
     # get devmodew object containing monitor information
-    settings = win32api.EnumDisplaySettings(primary_monitor.DeviceName, -1)
+    devmodew = win32api.EnumDisplaySettings(primary_monitor.DeviceName, win32con.ENUM_CURRENT_SETTINGS)
 
     # check current refresh rate
-    ref_rate = settings.DisplayFrequency
+    ref_rate = devmodew.DisplayFrequency
 
     # set variable for new refresh rate
     # if refresh rate is equal to 144hz, change it to 60hz and vice versa
@@ -59,89 +55,87 @@ def change_refresh_rate():
     else:
         new_ref_rate = 144
 
-    # execute of the refresh rate change
-    # get the devmode object
-    devmode = pywintypes.DEVMODEType()
-
     # assign variable to settings options
-    devmode.DisplayFrequency = new_ref_rate
+    devmodew.DisplayFrequency = new_ref_rate
 
     # fill the options field of devmode object
-    devmode.Fields = win32con.DM_DISPLAYFREQUENCY
+    devmodew.Fields = win32con.DM_DISPLAYFREQUENCY
 
-    # use windows api to pass the display setting written to devmode object
-    win32api.ChangeDisplaySettings(devmode, 0)
+    # use windows api to pass the display settings written to devmode object
+    win32api.ChangeDisplaySettings(devmodew, 0)
 
 
 def rotate_screen():
-    # get list of tuples containing notably the handle object and screen resolution information for each monitor
-    monitors = win32api.EnumDisplayMonitors()
+    # get displaydevice object of the primary monitor
+    primary_monitor = win32api.EnumDisplayDevices()
 
-    # for each monitor tuple extract handle object
-    for monitor in monitors:
-        handle, _, _ = monitor
+    # get devmodew object containing monitor information
+    devmodew = win32api.EnumDisplaySettings(primary_monitor.DeviceName, win32con.ENUM_CURRENT_SETTINGS)
 
-        # get monitor information for given handle object
-        monitor_info = win32api.GetMonitorInfo(handle)
+    # swap screen widths and heights between each other
+    devmodew.PelsWidth, devmodew.PelsHeight = devmodew.PelsHeight, devmodew.PelsWidth
 
-        # check if monitor is primary and its device name
-        device = monitor_info['Device']
-        is_primary = monitor_info['Flags'] == 1
+    # if the display orientation is equal to 0 (regular view) change the orientation value to 3 (rotate 270
+    # degrees left to get vertical view - personal preference)
+    if devmodew.DisplayOrientation == 0:
+        devmodew.DisplayOrientation = 3
+    else:
+        devmodew.DisplayOrientation = 0
 
-        if is_primary:
-            # get devmodew object for primary monitor
-            devmodew = win32api.EnumDisplaySettings(device, win32con.ENUM_CURRENT_SETTINGS)
-
-            # swap screen widths and heights between each other
-            devmodew.PelsWidth, devmodew.PelsHeight = devmodew.PelsHeight, devmodew.PelsWidth
-
-            # if the display orientation is equal to 0 (regular view) change the orientation value to 3 (rotate 270
-            # degrees left to get vertical view - personal preference)
-            if devmodew.DisplayOrientation == 0:
-                devmodew.DisplayOrientation = 3
-            else:
-                devmodew.DisplayOrientation = 0
-
-            # use windows api to pass the display setting written to devmode object
-            win32api.ChangeDisplaySettingsEx(device, devmodew)
+    # use windows api to pass the display settings written to devmode object
+    win32api.ChangeDisplaySettingsEx(primary_monitor.DeviceName, devmodew)
 
 
-def set_primary(idx, monitors_list, monitor_arrangement):
-    flag_for_primary = win32con.CDS_SET_PRIMARY | win32con.CDS_UPDATEREGISTRY | win32con.CDS_NORESET
-    flag_for_sec = win32con.CDS_UPDATEREGISTRY | win32con.CDS_NORESET
-    offset_x = - monitor_arrangement[idx][0]
-    offset_y = - monitor_arrangement[idx][1]
-    devices_num = len(monitors_list)
+def set_primary(index, monitors_list, monitor_arrangement):
+    # set flags for primary and secondary monitor
+    primary_flag = win32con.CDS_SET_PRIMARY | win32con.CDS_UPDATEREGISTRY | win32con.CDS_NORESET
+    secondary_flag = win32con.CDS_UPDATEREGISTRY | win32con.CDS_NORESET
 
-    for i in range(devices_num):
-        devmode = win32api.EnumDisplaySettings(monitors_list[i].DeviceName, win32con.ENUM_CURRENT_SETTINGS)
-        devmode.Position_x = monitor_arrangement[i][0] + offset_x
-        devmode.Position_y = monitor_arrangement[i][1] + offset_y
-        if (win32api.ChangeDisplaySettingsEx(monitors_list[i].DeviceName, devmode,
-                                             flag_for_sec if i != idx else flag_for_primary) !=
-                win32con.DISP_CHANGE_SUCCESSFUL):
-            return False
+    # set the x and y offset to put monitors in place
+    offset_x = - monitor_arrangement[index][0]
+    offset_y = - monitor_arrangement[index][1]
+    monitors_num = len(monitors_list)
 
-    return win32api.ChangeDisplaySettingsEx() == win32con.DISP_CHANGE_SUCCESSFUL
+    # iterate over monitors changing their position and state between primary and secondary
+    for i in range(monitors_num):
+        # get devmodew object containing monitor information
+        devmodew = win32api.EnumDisplaySettings(monitors_list[i].DeviceName, win32con.ENUM_CURRENT_SETTINGS)
+
+        # set the x and y position of monitor
+        devmodew.Position_x = monitor_arrangement[i][0] + offset_x
+        devmodew.Position_y = monitor_arrangement[i][1] + offset_y
+
+        # use windows api to pass the display settings written to devmodew object
+        if i != index:
+            win32api.ChangeDisplaySettingsEx(monitors_list[i].DeviceName, devmodew, secondary_flag)
+        else:
+            win32api.ChangeDisplaySettingsEx(monitors_list[i].DeviceName, devmodew, primary_flag)
+
+    win32api.ChangeDisplaySettingsEx()
 
 
 def switch_primary():
-    devices = []
+
+    # create a list to store display device objects
+    display_devices = []
+
+    # iterate over display devices
     i = 0
     while True:
         try:
             device = win32api.EnumDisplayDevices(None, i, 0)
+            # if the display device is attached to desktop and running, add it to the list
             if device.StateFlags & win32con.DISPLAY_DEVICE_ATTACHED_TO_DESKTOP:
-                devices.append(device)
+                display_devices.append(device)
             i += 1
-        except:
+        # break the loop when there are no more display devices
+        except pywintypes.error:
             break
 
-    for dev in devices:
-        print("Name: ", dev.DeviceName)
-
+    # get a list of tuples containing notably the handle object and screen resolution information for each monitor
     monitors = win32api.EnumDisplayMonitors()
 
+    # get primary monitor index by iterating over the monitors list and checking if the Flags attribute is set to 1
     i = 0
     for monitor in monitors:
         handle, _, _ = monitor
@@ -151,6 +145,14 @@ def switch_primary():
             break
         i += 1
 
+    # set next monitor as primary (if last monitor is primary, set primary to first monitor -> index 0)
+    if i + 1 == len(monitors):
+        new_primary = 0
+    else:
+        new_primary = i + 1
+
+    # change the primary monitor to secondary and vice versa and also set monitor arrangement to be able to move the
+    # mouse cursor between monitors on the linked axis
     if i:
 
         monitor_arrangement = {
@@ -158,7 +160,8 @@ def switch_primary():
             1: (-1920, 0)
         }
 
-        set_primary(0, devices, monitor_arrangement)
+        set_primary(new_primary, display_devices, monitor_arrangement)
+
     else:
 
         monitor_arrangement = {
@@ -166,14 +169,14 @@ def switch_primary():
             1: (0, 0)
         }
 
-        set_primary(1, devices, monitor_arrangement)
+        set_primary(new_primary, display_devices, monitor_arrangement)
 
 
 def switch_brightness():
-    # # get list of tuples containing notably the handle object and screen resolution information for each monitor
+    # get a list of tuples containing notably the handle object and screen resolution information for each monitor
     monitors = win32api.EnumDisplayMonitors()
 
-    # get primary monitor index
+    # get primary monitor index by iterating over the monitors list and checking if the Flags attribute is set to 1
     i = 0
     for monitor in monitors:
         handle, _, _ = monitor
@@ -182,7 +185,7 @@ def switch_brightness():
             break
         i += 1
 
-    # get current brightness setting for primary monitor
+    # get current brightness settings for primary monitor
     curr_brightness = sbc.get_brightness()[i]
 
     # if the brightness is set to 100 - change it to 25, otherwise change it to 100
@@ -203,11 +206,11 @@ image = Image.open('logo.png')
 
 # create menu
 menu = (
-    MenuItem('Switch resolution', change_resolution),
-    MenuItem('Switch refresh rate', change_refresh_rate),
-    MenuItem('Switch primary monitor', switch_primary),
     MenuItem('Switch brightness', switch_brightness),
-    MenuItem('Rotate screen', rotate_screen),
+    MenuItem('Switch orientation', rotate_screen),
+    MenuItem('Switch primary monitor', switch_primary),
+    MenuItem('Switch refresh rate', change_refresh_rate),
+    MenuItem('Switch resolution', change_resolution),
     MenuItem('Quit', on_quit)
 )
 
